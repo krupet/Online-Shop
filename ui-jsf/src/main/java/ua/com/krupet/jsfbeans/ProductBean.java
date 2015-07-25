2cad4e886ce1812360ff946690907edfcdd0151c
krupet <petrikpjatochkin@gmail.com>
2015-07-25 19:06:20 +0300

    admin/products delete product simse to bee working

--- ui-jsf/src/main/java/ua/com/krupet/jsfbeans/ProductBean.java
+++ ui-jsf/src/main/java/ua/com/krupet/jsfbeans/ProductBean.java
@@ -46,7 +46,7 @@ public class ProductBean implements Serializable{
         if (productService.editProduct(product) != null) {
             success = true;
             message = new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Updated!", "product with ID (" + product.getId() + ")updated successfully!");
+                    "Updated!", "product with ID (" + product.getId() + ") updated successfully!");
         } else message = new FacesMessage(FacesMessage.SEVERITY_WARN,
                         "Error", "error during updating product with ID (" + product.getId() + ")");
 
@@ -64,6 +64,7 @@ public class ProductBean implements Serializable{
         newProduct.setCreationDate("" + new Date().getTime());
         if (productService.postProduct(newProduct) != null) {
             success = true;
+            productList.add(newProduct); // TODO: just anthill lazy loading!
             message = new FacesMessage(FacesMessage.SEVERITY_INFO,
                     "Posted!", "product posted successfully!");
         } else message = new FacesMessage(FacesMessage.SEVERITY_WARN,
@@ -71,6 +72,22 @@ public class ProductBean implements Serializable{
 
         FacesContext.getCurrentInstance().addMessage(null, message);
         context.addCallbackParam("success", success);
+    }
+
+    public void deleteProduct(Product product) {
+        FacesMessage message = null;
+
+        int index = productList.indexOf(product);
+        String prodID = product.getId();
+        Product deletedProduct = productService.removeProduct(product);
+        if (deletedProduct.getId() == null) {
+            productList.remove(index); // TODO: just anthill lazy loading!
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Deleted!", "product with ID (" + prodID + ") deleted successfully!");
+        } else message = new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        "Error", "error during removing product with ID (" + prodID + ")");
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
     public Product getProduct() {
