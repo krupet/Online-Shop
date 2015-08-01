0bcc429b98364e55b9f98b868f7b0a00d2f46408
krupet <krutsiuk.petr@gmail.com>
2015-08-01 12:29:25 +0300

    BUG: gained by orderDAO updateOrder() - rewrites products in DB; usr/shop & usr/usrinfo base implemented

--- ui-jsf/src/main/java/ua/com/krupet/config/JSFBeansConfig.java
+++ ui-jsf/src/main/java/ua/com/krupet/config/JSFBeansConfig.java
@@ -3,10 +3,7 @@ package ua.com.krupet.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import ua.com.krupet.jsfbeans.OrderBean;
-import ua.com.krupet.jsfbeans.ProductBean;
-import ua.com.krupet.jsfbeans.UserBean;
-import ua.com.krupet.jsfbeans.UserInfoBean;
+import ua.com.krupet.jsfbeans.*;
 
 /**
  * Created by krupet on 7/22/15.
@@ -34,5 +31,10 @@ public class JSFBeansConfig {
     @Bean
     public UserInfoBean userInfoBean() {
         return new UserInfoBean();
+    }
+
+    @Bean
+    public ShopBean shopBean() {
+        return new ShopBean();
     }
 }
