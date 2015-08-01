b8fac5676ac59b778493fc8f8b6e4adfa7e30b76
krupet <krutsiuk.petr@gmail.com>
2015-08-01 15:18:00 +0300

    spring security JDBC authentication provided

--- ui-jsf/src/main/java/ua/com/krupet/config/SecurityConfig.java
+++ ui-jsf/src/main/java/ua/com/krupet/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package ua.com.krupet.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 /**
  * Created by krupet on 7/26/15.
@@ -14,6 +17,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -59,17 +65,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //Configure roles and passwords as in-memory authentication
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
-        auth.inMemoryAuthentication()
-                .withUser("manager")
-                .password("manager")
-                .roles("MANAGER");
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("user")
-                .roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("admin")
+//                .roles("ADMIN");
+//        auth.inMemoryAuthentication()
+//                .withUser("manager")
+//                .password("manager")
+//                .roles("MANAGER");
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("user")
+//                .roles("USER");
+
+//        JDBC authentication
+//        auth.jdbcAuthentication().dataSource(dataSource)
+//        .usersByUsernameQuery(
+//                "select username,password, enabled from users where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, role from user_roles where username=?");
+
+        /*
+            dirty hack: user must contain 'enabled' column so instead i selecting 1
+            TODO: make all properly (provide 'enabled' column or UserDetailsService)
+         */
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select user_login, user_password, 1 from users where user_login=?")
+                .authoritiesByUsernameQuery(
+                        "select user_name, user_role from user_roles where user_name=?");
     }
 }
